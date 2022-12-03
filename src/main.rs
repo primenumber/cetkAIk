67285fece55258107baaa25f3f7c22a2dab84aa2
primenumber <prime@kmc.gr.jp>
2022-12-03 17:43:34 +0900

    Implement do_match

--- src/main.rs
+++ src/main.rs
@@ -9,26 +9,21 @@ use greedy::*;
 use random_player::*;
 use cetkaik_engine::*;
 
-fn main() {
-    let config = Config::cerke_online_alpha();
-    let pstate = initial_state();
-    //eprintln!("{:?}", pstate);
-    let (mut state, _) = pstate.choose();
-    let mut ia_searcher = GreedyPlayer::new(config);
-    let mut a_searcher = GreedyPlayer::new(config);
+fn do_match(config: Config, ia_player: &mut dyn CetkaikEngine, a_player: &mut dyn CetkaikEngine) {
+    let mut state = initial_state().choose().0;
     let mut turn_count = 0;
     loop {
-        println!("{} {:?} {} {}", turn_count, state.season, state.scores.ia(), state.scores.a());
+        println!("Turn: {}, Season: {:?}, Scores: (IA:{}, A:{})", turn_count, state.season, state.scores.ia(), state.scores.a());
         let searcher: &mut dyn CetkaikEngine = match state.whose_turn {
-            Side::IASide => &mut ia_searcher,
-            Side::ASide => &mut a_searcher,
+            Side::IASide => ia_player,
+            Side::ASide => a_player,
         };
         let pure_move = searcher.search(&state);
         if pure_move.is_none() {
             break;
         }
         let pure_move = pure_move.unwrap();
-        println!("{:?}", pure_move);
+        println!("Move: {:?}", pure_move);
         let hnr_state = match pure_move {
             PureMove::NormalMove(m) => {
                  apply_normal_move(&state, m, config).unwrap().choose().0
@@ -36,7 +31,7 @@ fn main() {
             PureMove::InfAfterStep(m) => {
                 let ext_state = apply_inf_after_step(&state, m, config).unwrap().choose().0;
                 let aha_move = searcher.search_excited(&ext_state).unwrap();
-                println!("{:?}", aha_move);
+                println!("Move(excited): {:?}", aha_move);
                 apply_after_half_acceptance(&ext_state, aha_move, config).unwrap().choose().0
             }
         };
@@ -69,4 +64,11 @@ fn main() {
         }
         turn_count += 1;
     }
+}
+
+fn main() {
+    let config = Config::cerke_online_alpha();
+    let mut ia_searcher = RandomPlayer::new(config);
+    let mut a_searcher = GreedyPlayer::new(config);
+    do_match(config, &mut ia_searcher, &mut a_searcher);
 }
